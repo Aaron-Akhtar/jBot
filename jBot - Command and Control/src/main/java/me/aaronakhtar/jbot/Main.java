37b7ce69240d9e08140ccd80dd1c8778f1e61b1f
Aaron <media@aaronakhtar.me>
2021-08-02 23:46:31 +0100

    jBot Malware & CNC initial commit (PRE-DEV)

---
+++ jBot - Command and Control/src/main/java/me/aaronakhtar/jbot/Main.java
@@ -0,0 +1,18 @@
+package me.aaronakhtar.jbot;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //TODO:
+        //  create server
+        //  create client handlers
+        //  create bot handler
+        //
+        //  So much more as well, however these
+        //  are the primary concerns at the moment.
+
+
+    }
+
+}
